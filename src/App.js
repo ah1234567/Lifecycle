d8d718fc35664899ef54a82efd426d7e304b3f51
Student <jsitbon@qcc.cuny.edu>
2019-07-10 17:28:20 -0400

    lifecycle

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+class App extends Component{
+  constructor(){
+    super();
+    this.state = {
+      items: [],
+      isloaded: false,
+    }
+  }
+  
+  componenetDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    .then(res => {
+
+      return res.json();
+    })
+
+
+    .then(data => {
+      console.log(data);
+      this.setState({
+        isloaded: true,
+        items: data,
+      })
+    })
+
+
+  }
+
+
+  render(){
+    var { isloaded, items} = this.state;
+    if(!isloaded){
+
+      return <div>Loading...</div>
+    }else
+    return(
+      <div className= "App">
+      <div className = "Names">
+      <ul>
+        {items.map( el => {
+          return(
+
+            <li key={el.id}>
+            Name: {el.name} | UserName: {el.username} | {` `}<a href = {`https://${el.website}`}> Website</a>
+            </li>
+          )
+        })}
+      </ul>
+      </div>
+      </div>
+    )
+  }
 }
-
 export default App;
